7a71f9a052de3dd0592a2252dc09d6c450899a4b
RiditaNizam <riditan@gmail.com>
2017-05-14 20:12:40 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Right2 right2Object = new Right2();
+    }
+}
---
+++ src/com/company/Right2.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by ridita on 5/14/17.
+ */
+public class Right2 {
+    public String right2(String str) {
+        return str.substring(str.length() - 2) + str.substring(0, str.length() - 2);
+    }
+}
